80163020dd37133bcb469f17a48791b2f2a0d7fd
godswillakaba <godswillakaba@gmail.com>
2025-06-19 03:20:34 +0000

    week7

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running ("03 Nov 2024", 30, 3.0),
+            new Cycling ("03 Nov 2024", 45, 15.0),
+            new Swimming ("03 Nov 2024", 30, 20)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary()); 
+        }
     }
 }
---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,24 @@
+using System;
+public class Swimming : Activity
+{
+    private int _laps;
+    public Swimming(string date, int length, int laps) : base(date, length)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50 / 100.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetLength()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetLength() / GetDistance();
+    }
+}
